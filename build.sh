b425f0a25011a516a1ed88669de15a00ce111702
Kristjan Rang <mail@rang.ee>
2015-01-30 17:56:09 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ docker build -t pebbles/pebblerunner ../pebblerunner
 echo "-----> Building Mike"
 cd ..
 ROOT=$(pwd)
-cd mike/app
-docker rm -f mike-build 2&> /dev/null
-git archive master | docker run -i -a stdin --name mike-build -e CURL_TIMEOUT=600 -e DBNAME=docker -e DBUSER=docker -e DBPASS=docker --link mike-redis:redis --link mike-postgresql:db -v $ROOT/mike/cache:/tmp/cache:rw pebbles/pebblerunner build > /dev/null
+cd mike
+docker rm -f mike-build > /dev/null
+git archive master | docker run -i -a stdin --name mike-build -e CURL_TIMEOUT=600 -e DBNAME=postgres -e DBUSER=postgres -e DBPASS=dbpass --link mike-redis:redis --link mike-postgresql:db pebbles/pebblerunner build > /dev/null
 docker logs -f mike-build
 docker commit mike-build pebbles/mike
 docker rm mike-build
 cd $ROOT/pebblescape
 
 echo "-----> Building receiver"
-# docker build -t pebbles/receiver ../receiver
+docker build -t pebbles/receiver ../receiver
 
--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "-----> Starting Mike dependencies"
-docker run -d --name mike-redis pebbles/redis 2&> /dev/null
-docker run -d --name mike-postgresql pebbles/postgresql 2&> /dev/null
-docker start mike-redis mike-postgresql 2&> /dev/null
-# start etcd here
+docker run -d --name mike-redis redis > /dev/null
+docker run -d --name mike-postgresql -e POSTGRES_PASSWORD=dbpass postgres > /dev/null
+docker run -d --name etcd -p 4001:4001 -p 7001:7001 coreos/etcd > /dev/null
+docker start mike-redis mike-postgresql etcd > /dev/null
